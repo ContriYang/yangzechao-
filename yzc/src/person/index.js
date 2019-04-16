a7e55da31fbf9f1cf10c33614bef11597a704723
ContriYang <1341405214@qq.com>
2019-04-16 17:40:53 +0800

    second

--- yzc/src/person/index.js
+++ yzc/src/person/index.js
@@ -1,13 +1,23 @@
 $(function(){
+	//操作dom
 	//鼠标移入显示二级目录
-	
-		$(".na_li").hover(function(){
-			$(".nav_ul").fadeOut();
-			$(this).find(".nav_ul").fadeIn();
-		},
-		function(){
-			console.log(123)
-			$(this).find(".nav_li").fadeOut();
-		})
+	$(".na_li").hover(function(){
+		$(".nav_ul").fadeOut();
+		//计算高度
+		//这里可以使用css3的方法来代替
+		nHeight = $(this).find(".nav_ul li").length * 0.8;
+		$(this).find(".nav_ul").css("display","block").animate({height:nHeight + "rem"});
+	},
+	function(){
+		$(this).find(".nav_ul").css("display","none").animate({height:"0rem"});
+	})
+	//主页轮播图
+	WSCMobileSlide({
+		"WSCSlideTransition": "WSCSlideTransition",
+		"WSCSlideWrapper": $(".WSCSlideWrapper"),
+		"WSCSlide": $(".WSCSlide"),
+		"WSCSlide_img": $(".WSCSlide_img"),
+		"timerNum": 3000
+	});
 	
 })
